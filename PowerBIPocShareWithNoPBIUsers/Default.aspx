f2e96d390f10af0e91784886437bcb1f3c72d2b3
PLAINCONCEPTS\djimenez <djimenez@plainconcepts.com>
2017-07-03 23:06:38 +0200

    Subo demo

---
+++ PowerBIPocShareWithNoPBIUsers/Default.aspx
@@ -0,0 +1,66 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="PowerBIPocShareWithNoPBIUsers.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <link rel="stylesheet" href="/css/master.css" type="text/css" />
+    <title>Power BI - Example Report Embedding</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <script type="text/javascript" src="scripts/powerbi.js"></script>
+
+            <script type="text/javascript">
+                window.onload = function () {
+                    var accessToken = document.getElementById('accessToken').value;
+
+                    if (!accessToken || accessToken == "") {
+                        return;
+                    }
+
+                    var embedUrl = document.getElementById('LblEmbedUrl').innerText;
+                    var reportId = document.getElementById('LblReportId').innerText;
+                    
+                    var models = window['powerbi-client'].models;
+                    var config = {
+                        type: 'report',
+                        tokenType: models.TokenType.Aad,
+                        accessToken: accessToken,
+                        embedUrl: embedUrl,
+                        id: reportId
+                    };
+
+                    var reportContainer = document.getElementById('reportContainer');
+
+                    var report = powerbi.embed(reportContainer, config);
+                };
+            </script>
+
+            <asp:HiddenField ID="accessToken" runat="server" />
+
+            <h1>Demo Power BI to Embed Report</h1>
+
+            <div>
+                Report Name:
+                <asp:Label ID="LblReportName" runat="server"></asp:Label>
+            </div>
+
+            <div>
+                Report Id:
+                <asp:Label ID="LblReportId" runat="server"></asp:Label>
+            </div>
+
+            <div>
+                Report Embed URL:
+                <asp:Label ID="LblEmbedUrl" runat="server"></asp:Label>
+            </div>
+
+            <div>
+                <div id="reportContainer" style="width:900px; height:500px"></div>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ PowerBIPocShareWithNoPBIUsers/Default.aspx.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Newtonsoft.Json;
+using System;
+using System.Globalization;
+using System.Net;
+
+namespace PowerBIPocShareWithNoPBIUsers
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Session["AccessToken"] = GetAuthToken();
+            if (Session["AccessToken"] != null)
+            {
+                accessToken.Value = Session["AccessToken"].ToString();
+
+                GetReport();
+            }
+        }
+
+        protected void GetReport()
+        {
+            string baseUriAPI = "https://api.powerbi.com/v1.0/myorg";
+            WebRequest request = WebRequest.Create(
+                String.Format("{0}/reports",
+                baseUriAPI)) as HttpWebRequest;
+
+            /*WebRequest request = WebRequest.Create(
+                String.Format("{0}/groups/groupId/reports",
+                baseUriAPI)) as HttpWebRequest;*/
+
+            request.Method = "GET";
+            request.ContentLength = 0;
+            request.Headers.Add("Authorization", String.Format("Bearer {0}", accessToken.Value));
+
+            using (var response = request.GetResponse() as HttpWebResponse)
+            {
+                using (var reader = new System.IO.StreamReader(response.GetResponseStream()))
+                {
+                    PBIReports reports = JsonConvert.DeserializeObject<PBIReports>(reader.ReadToEnd());
+                    if (reports.value.Length > 0)
+                    {
+                        var report = reports.value[2];
+
+                        LblEmbedUrl.Text = report.embedUrl;
+                        LblReportId.Text = report.id;
+                        LblReportName.Text = report.name;
+                    }
+                }
+            }
+        }
+
+        public string GetAuthToken()
+        {
+            AuthenticationResult result = null;
+            try
+            {
+                string aadInstance = "https://login.microsoftonline.com/{0}";
+                string tenant = "contoso.onmicrosoft.com";
+                string authority = String.Format(CultureInfo.InvariantCulture, aadInstance, tenant);
+                AuthenticationContext authContext = new AuthenticationContext(authority);
+
+                string resourceId = "https://analysis.windows.net/powerbi/api";
+                string clientId = "a7c98fc1-098b-4561-b0c6-a9939f79f563";
+                string hardcodedUsername = "myuser@contoso.onmicrosoft.com";
+                string hardcodedPassword = "mypassword";
+
+                result = authContext.AcquireToken(resourceId, clientId, new UserCredential(hardcodedUsername, hardcodedPassword));
+            }
+            catch (AdalException ex)
+            {
+            }
+
+            string token = result.AccessToken;
+            return token;
+        }
+    }
+
+    public class PBIReports
+    {
+        public PBIReport[] value { get; set; }
+    }
+    public class PBIReport
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string webUrl { get; set; }
+        public string embedUrl { get; set; }
+    }
+}
